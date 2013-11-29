d40b8f4b47732bdd0f2ba84ac98e0e11a23d1114
Raphael Eidus <raphaeleidus@gmail.com>
2013-11-28 19:18:16 -0500

    WordPress comment block

---
+++ qr-redirector.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: QR Redirector
+ * Plugin URI: http://github.com/raphaeleidus/wordpress-qr-redirector
+ * Description: Setup a set of reusable QR codes that can be redirected to a desired url.
+ * Version: 0.1
+ * Author: Raphael Eidus
+ * Author URI: http://raphaeleidus.com/
+ * License: GPL2
+ */
+ ?>
